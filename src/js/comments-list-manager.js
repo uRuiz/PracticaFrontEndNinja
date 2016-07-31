879e2a3f072d8aabdb86280ff849b61bcee49f3f
Urko Ruiz <zaszur@gmail.com>
2016-07-31 15:52:24 +0200

    Mejorada la carga de comentarios y dado formato a los comentarios

--- src/js/comments-list-manager.js
+++ src/js/comments-list-manager.js
@@ -5,14 +5,19 @@ var utils = require("./utils");
 module.exports = {
 
 	load: function() {
-
+		$(document).ready(function(){
+			apiClient.list(function(response) {
+				$('.comments-count span').html('');				
+				$('.comments-count span').append(response.length);
+			})
+		});
 		commentsLoaded = false;
 		// Petición AJAX para cargar la lista de comentarios
 		$(window).scroll(function(){
 			if (($(window).scrollTop() > $(".social-block").offset().top + $(".social-block").outerHeight() - $(window).height()) && (!commentsLoaded)){
 				apiClient.list(function(response) {
 					$('.comments-list').html('');
-					$('.comments-count span').html('');
+					//$('.comments-count span').html('');
 					for (var i in response) {
 						var comment = response[i];
 
@@ -21,13 +26,15 @@ module.exports = {
 						var email = comment.email || "";
 						var comentario = comment.comentario || "";
 
-						var html = '<div class="user-full-name"> ' + utils.escapeHTML(nombre) + ' ' + utils.escapeHTML(apellido) + '</div">';
-		                html += '<div class="user-email"> ' + utils.escapeHTML(email) + '</div">';
+						var html = '<div class="user-comment">';
+						html += '<div class="user-information">';
+						html += '<div class="user-full-name"> ' + utils.escapeHTML(nombre) + ' ' + utils.escapeHTML(apellido) + '</div>';
+		                html += '<div class="user-email"> ' + utils.escapeHTML(email) + '</div></div>';
 		                html += '<div class="comentario">' + utils.escapeHTML(comentario) + '</div>';
-		                html += '</article>';
+		                html += '</div>';
 		                $('.comments-list').append(html);
 					}
-					$('.comments-count span').append(parseInt(i) + 1);
+					//$('.comments-count span').append(parseInt(i) + 1);
 				}, function(response){
 					console.error("ERROR", response);
 				}); 
